2f03c145986f083144dd0f595459641dc3977337
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2023-03-25 15:50:55 +0100

    [UPDATE] implement protocol

--- socket.cpp
+++ socket.cpp
@@ -3,55 +3,67 @@
 #include "socket.h"
 #include "wifi_connect.h"
 
-void establish_socket(){
+void establish_socket()
+{
   char message[255];
   IPAddress gateway = WiFi.gatewayIP();
   sprintf(message, "Establishing socket to server : %s, at port : %d", gateway.toString(), PORT_NUMBER);
   Serial.println(message);
 
-  while (!client.connect("192.168.1.14", 5000)) {
+  while (!client.connect(gateway.toString(), PORT_NUMBER))
+  {
     delay(500);
     Serial.println("Can't connect");
   }
   Serial.println("Connected to server !");
-
 }
 
-char *construct_pair_message(){
-  char *pair_message = (char *)malloc(64 * sizeof(char));
+char *construct_pair_message()
+{
   IPAddress ip_address = get_ip_address();
-  char* mac_address = get_mac_address();
+  String ip_address_string = ip_address.toString();
+  unsigned int ip_address_length = ip_address_string.length() + 1;
+  unsigned int nb = ip_address_length > 0 ? (int)log10((double)ip_address_length) + 1 : 1;
 
-  sprintf(pair_message, "{\"action\":\"pair\",\"ip_address\":\"%s\",\"id\":\"%s\"}",ip_address.toString(), mac_address);
+  char *pair_message = (char *)malloc((63 + nb + ip_address_length) * sizeof(char));
+  char *mac_address = get_mac_address();
+
+  sprintf(pair_message, "*%d {\"action\":\"pair\",\"ip_address\":\"%s\",\"mac_address\":\"%s\"}", 61 + ip_address_length, ip_address_string, mac_address);
   return pair_message;
 }
 
-bool check_protocol(){
-   char check = client.read();
-    if(check == PROTOCOL_CHECK)
-      return true;
-    else{
-      // The message is not processed
-      while (client.available()) client.read();
-      return false;
-    }
+bool check_protocol()
+{
+  char check = client.read();
+  if (check == PROTOCOL_CHECK)
+    return true;
+  else
+  {
+    // The message is not processed
+    while (client.available())
+      client.read();
+    return false;
+  }
 }
 
-char *get_response_data(){
+char *get_response_data()
+{
   char carac;
   char length_as_string[5];
   int i = 0;
 
   // Get the length of the data
-  while((carac = client.read()) != ' ') length_as_string[i++] = carac;
+  while ((carac = client.read()) != ' ')
+    length_as_string[i++] = carac;
   length_as_string[i] = '\0';
   int length = atoi(length_as_string);
 
   // Retrieve data
-  char *content = (char*)malloc((length+1) * sizeof(char));
-  for(int i = 0; i < length; i++){
+  char *content = (char *)malloc((length + 1) * sizeof(char));
+  for (int i = 0; i < length; i++)
+  {
     content[i] = client.read();
   }
-  content[length]= '\0';
+  content[length] = '\0';
   return content;
 }
